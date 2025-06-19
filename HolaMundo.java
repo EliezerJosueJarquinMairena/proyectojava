a15e55a241efdf2292bf7c08bd930efbbca85e83
Eliezer Josué Jarquin Mairena <eliezer.jarquin405@std.uni.edu.ni>
2025-06-19 14:46:35 -0600

    Agregamos la multiplicación

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,15 @@
 public class HolaMundo{
 public static void main(String[] args) {
 String nombre = "Estudiante";
-int a = 5;
-int b = 3;
+int a = 3;
+int b = 8;
 int suma = a + b;
+int multiplicacion = a*b;
+int resta = a -b;
+
 System.out.println("Hola, " + nombre + "!");
 System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+System.out.println("La resta de  " + a + " - " + b + " es: " + resta);
+System.out.println("La multiplicacion de "  a + " * " + b + " es: " + multiplicacion);
 }
 }
